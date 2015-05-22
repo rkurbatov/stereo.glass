5e9a8cf1f329c5043778e2f5726a6905bfc5770f
Roman Kurbatov <rkurbatov@gmail.com>
2015-05-22 16:38:03 +0200

    Added: Compass support

--- app/routes/base.js
+++ app/routes/base.js
@@ -4,12 +4,12 @@ module.exports = function (express, Account) {
     var Router = express.Router();
 
     // server routes ===========================================================
-    /*Router.get(['/'], function(req, res, next){
-        res.render('admin/index', { url: req.originalUrl });
-    });*/
+    Router.get(['/'], function(req, res, next){
+        res.render('main/index', { user: req.user });
+    });
 
     // admin interface
-    Router.get(['/', '/admin'], function (req, res, next) {
+    Router.get(['/admin'], function (req, res, next) {
         if (req.isAuthenticated()) {
             if (req.user._id) {
                 Account.findByIdAndUpdate(req.user._id, {activeAt: new Date}, function (err) {
