572cb01a991a954cbced3b107e154f1ae528bfa5
Roman Kurbatov <rkurbatov@gmail.com>
2015-07-02 14:11:51 +0300

    [+] basic gulp integration

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var concat = require('gulp-concat');
+var exec = require('gulp-exec');
+
