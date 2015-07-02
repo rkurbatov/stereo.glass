bbd3dc549d353a5e8617bdb299d85ab4d0d8b52f
Roman Kurbatov <rkurbatov@gmail.com>
2015-07-02 18:54:12 +0300

    [+] additional gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,5 @@ var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 var concat = require('gulp-concat');
 var exec = require('gulp-exec');
-
+var notify = require('gulp-notify');
+var connect = require('gulp-connect-pm2');
