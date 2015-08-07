f0c499ac37bc77610935962c3afa7680cd1399ca
Roman Kurbatov <rkurbatov@gmail.com>
2015-08-08 02:34:41 +0300

    SG-20 Использовать bluebird/PromisifyAll для mongoose (/api/messages)

--- app/routes/api-messages.js
+++ app/routes/api-messages.js
@@ -2,7 +2,6 @@ module.exports = function (express, Message) {
     'use strict';
 
     var Router = express.Router();
-    var _ = require('lodash');
 
     // DECLARATION
 
@@ -14,64 +13,62 @@ module.exports = function (express, Message) {
     // IMPLEMENTATION
 
     function postMessage(req, res) {
-        if (req.isAuthenticated()) {
-
-            var message = req.body.params.message;
-            if (!message) {
-               return res.status(400).json({status: 'error', message: "Bad request!"}); 
-            }
-
-            Message.create(message, function (err) {
-                if (err) {
-                    console.log('Error creating message! ' + err);
-                    res.status(500).json({status: 'error', message: err});
-                }
-                else {
-                    res.status(201).json({ status: 'success' });
-                }
-            });
-
-        } else {
-            res.status(403).send('forbidden').end();
+        if (!req.isAuthenticated()) {
+            return res.sendStatus(403);
         }
+
+        var message = req.body.params.message;
+        if (!message) {
+            return res.sendStatus(400);
+        }
+
+        Message
+            .create(message)
+            .then(function () {
+                return res.sendStatus(201);
+            })
+            .catch(function (err) {
+                console.log('Error creating message! ' + err);
+                return res.sendStatus(500);
+            });
     }
 
     function postMessagesSearchByUserAndRoles(req, res) {
-        if (req.isAuthenticated()) {
-            var user = req.params.user;
-            var groups = req.params.groups;
-            if (groups && !Array.isArray.groups) {
-                groups = [groups];
-            }
-
-            var conditions;
-            var condUser = { toUser: user };
-            var condGroup = { toGroup: { $in: groups } };
-
-            if (!user && !groups) {
-                return res.status(400).json({ status: 'error', message: 'Bad request!' });
-            }
-            if (user && groups) {
-                conditions = { $or: [condUser, condGroup] };
-            } else if (!user){
-                conditions = condGroup;
-            } else if (!groups) {
-                conditions = condUser;
-            }
-            
-            Message.find(conditions, function(err, messages){
-                if (err) {
-                    console.log('Error creating message! ' + err);
-                    res.status(400).json({status: 'error', message: err});
-                }
-
-                res.status(200).json(messages);
-            });
-
-        } else {
-            res.status(403).send('forbidden').end();
+        if (!req.isAuthenticated()) {
+            return res.sendStatus(403);
         }
 
+        var user = req.params.user;
+        var groups = req.params.groups;
+        if (!user && !groups) {
+            return res.sendStatus(400);
+        }
+
+        if (groups && !Array.isArray.groups) {
+            groups = [groups];
+        }
+
+        var conditions;
+        var condUser = {toUser: user};
+        var condGroup = {toGroup: {$in: groups}};
+
+        if (user && groups) {
+            conditions = {$or: [condUser, condGroup]};
+        } else if (!user) {
+            conditions = condGroup;
+        } else if (!groups) {
+            conditions = condUser;
+        }
+
+        Message
+            .find(conditions)
+            .then(function (messages) {
+                return res.json(messages);
+            })
+            .catch(function (err) {
+                console.log('Error creating message! ' + err);
+                return res.sendStatus(500);
+            });
     }
 
 };
