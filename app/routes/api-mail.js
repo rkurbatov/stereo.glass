6ac2ba3bad65dc06b3a81afb159576627035977e
Roman Kurbatov <rkurbatov@gmail.com>
2015-08-08 01:20:17 +0300

    SG-20 Использовать bluebird/PromisifyAll для mongoose (/api/files, /api/mail)

--- app/routes/api-mail.js
+++ app/routes/api-mail.js
@@ -11,39 +11,37 @@ module.exports = function (express, mailer) {
 
     function postMail(req, res) {
 
-        if (req.isAuthenticated()) {
+        if (!req.isAuthenticated()) {
+            return res.sendStatus(403);
+        }
 
-            var params = req.body.params;
-            var mail = params.mail;
-            var vars = params.vars;
+        var params = req.body.params;
+        var mail = params.mail;
+        var vars = params.vars;
 
-            if (!mail.to || !mail.subject || !mail.template) {
-                console.log('Error in mailing request!');
-                return res.status(400).json({status: 'error', message: 'Error in mailing request!'})
+        if (!mail.to || !mail.subject || !mail.template) {
+            console.log('Error in mailing request!');
+            return res.sendStatus(400);
+        }
+
+        // Setup email data.
+        var mailOptions = {
+            to: mail.to,
+            subject: mail.subject
+        };
+
+        mailOptions = _.merge(mailOptions, vars);
+
+        // TODO: template file checking
+        mailer.send('mail-templates/' + mail.template, mailOptions, mailerCallback)
+
+        function mailerCallback(err) {
+            if (err) {
+                console.log('Mailer error! ', err);
+                return res.sendStatus(500);
             }
-
-            // Setup email data.
-            var mailOptions = {
-                to: mail.to,
-                subject: mail.subject
-            };
-
-            mailOptions = _.merge(mailOptions, vars);
-
-            console.log('email is: ', mailOptions);
-
-            // TODO: template file checking
-            mailer.send('mail-templates/' + mail.template, mailOptions, function (err) {
-                if (err) {
-                    console.log("Mailer error: ", err);
-                    return res.status(500).send({status: 'error', message: 'forbidden'});
-                } else {
-                    return res.status(200).send({status: 'success'});
-                }
-            })
-
-        } else {
-            return res.status(403).send('forbidden');
+            console.log('Mail is sent to ', mailOptions.to);
+            return res.sendStatus(200);
         }
     }
 
