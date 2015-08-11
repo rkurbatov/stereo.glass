a8e18e112582cf509a1a4109644bd5f2649f5615
Roman Kurbatov <rkurbatov@gmail.com>
2015-08-11 15:13:08 +0300

    Profile dropdown

---
+++ public/app/adminpage/languages/Languages.controller.js
@@ -0,0 +1,25 @@
+(function (window, angular, undefined) {
+    'use strict';
+
+    angular
+        .module('sgAppAdmin')
+        .controller('Languages', Languages);
+
+    Languages.$inject = [];
+    function Languages() {
+
+        //===== DECLARATION =====
+        var vm = this;
+
+        initController();
+
+        //=== IMPLEMENTATION ====
+
+        function initController() {
+
+        }
+
+
+    }
+
+})(window, window.angular);
