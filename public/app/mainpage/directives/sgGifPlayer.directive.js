b975ae980f60c549bb7ebe070b70f920e28915a5
Roman Kurbatov <rkurbatov@gmail.com>
2015-09-16 02:55:03 +0300

    SG-192 Директива sg-gif-player

--- public/app/mainpage/directives/sgGifPlayer.directive.js
+++ public/app/mainpage/directives/sgGifPlayer.directive.js
@@ -12,22 +12,37 @@
         return {
             restrict: 'C',
             scope: {},
-            template: '<img ng-click="switchState()" ng-src="{{imgSrc}}"/><i class="fa fa-play-circle-o" ng-click="switchState()" ng-class="{invisible: state !== \'static\'}"></i>',
+            template: `
+            <img ng-click="switchState()"
+            ng-src="{{imgSrc}}" sg-on-img-load="endSwitchState()"/>
+            <i class="fa fa-play-circle-o"
+            ng-click="switchState()"
+            ng-class="{invisible: state !== 'static'}"></i>
+            <i class="fa fa-spinner fa-pulse"
+            ng-class="{invisible: state !== 'loading'}"></i>
+            `,
             link
         };
 
         function link(scope, elm, attrs) {
-            scope.state = 'static';
+            var nextState = 'static';
+            scope.state = 'loading';
             scope.imgSrc = attrs.static;
-            scope.switchState = function() {
+            scope.nextState = 'static';
+
+            scope.switchState = function () {
                 if (scope.state === 'static') {
-                    scope.state = 'anim';
+                    scope.nextState = 'anim';
                     scope.imgSrc = attrs.anim;
                 } else {
-                    scope.state = 'static';
+                    scope.nextState = 'static';
                     scope.imgSrc = attrs.static;
                 }
-            }
+                scope.state = 'loading';
+            };
+            scope.endSwitchState = function () {
+                scope.state = nextState;
+            };
         }
     }
 
