4980c6f756b4e2e1830b0052b2049abad90f5aff
Roman Kurbatov <rkurbatov@gmail.com>
2015-09-20 13:51:03 +0300

    Simplify switcher.

--- public/app/mainpage/directives/sgLangSwitcher.directive.js
+++ public/app/mainpage/directives/sgLangSwitcher.directive.js
@@ -18,27 +18,10 @@
         };
 
         function link(scope, elm, attrs) {
-            scope.IntSvc = sgIntSvc;
-            scope.switchLang = switchLang;
-            scope.toggleSwitcher = toggleSwitcher;
-            scope.closeSwitcher = closeSwitcher;
-            scope.isVisible = false;
-
-            function toggleSwitcher() {
-                /*var switcher = angular.element('.lang-switcher')[0];
-                angular.element(switcher).toggleClass('visible');*/
-                scope.isVisible = !scope.isVisible;
-            }
-
-            function closeSwitcher() {
-                console.log('isVisible: ', isVisible);
-                if (scope.isVisible) toggleSwitcher();
-            }
-
-            function switchLang(code) {
-                sgIntSvc.switchLang(code);
-                closeSwitcher();
-            }
+            _.extend(scope, {
+                isVisible: false,
+                sgIntSvc
+            });
         }
 
     }
