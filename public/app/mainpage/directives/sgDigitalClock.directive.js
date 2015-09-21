fb045585d1d9ba4565feb3f3da8a08c2b0097e5c
Roman Kurbatov <rkurbatov@gmail.com>
2015-09-21 18:14:26 +0300

    external checkout

--- public/app/mainpage/directives/sgDigitalClock.directive.js
+++ public/app/mainpage/directives/sgDigitalClock.directive.js
@@ -9,61 +9,36 @@
 
     // sets variable for widescreen displays
     function sgDigitalClock($interval) {
-
-        var template = `
-            <div class="sg-digital-clock" title="{{title}}">
-                <span class="background">{{blank}}</span>
-                <span class="timer"
-                ng-class="{'open': isOpen, 'closed': !isOpen}">{{time}}</span>
-                <div class="foreground"></div>
-            </div>
-        `;
-
         return {
             restrict: 'E',
-            template,
+            templateUrl: '/templates/directive/sgDigitalClock',
             scope: {
+                isOpen: '=',
                 timezone: '@',
-                holidaysHandler: '&'
+                title: '=',
+                onTimerClick: '&'
             },
             link
         };
 
-        function link(scope, elm, attrs) {
+        function link(scope, elm, attr) {
             var delimiter = true; // colon delimiter blinking every second
             scope.blank = '88:88';
-            scope.isOpen = true;
+            if (!attr.isOpen) {
+                scope.isOpen = true;
+            }
+            if (!attr.onTimerClick) {
+                scope.onTimerClick = ()=> angular.noop;
+            }
 
             $interval(setCurrentTime, 1000);
 
             function setCurrentTime() {
                 var curTime = moment().tz(scope.timezone || 'Europe/London');
                 var timeFormat = delimiter ? 'HH:mm' : 'HH mm';
-                checkTime(curTime);
+                scope.onTimerClick()(curTime);
                 scope.time = curTime.format(timeFormat); // check time for open-close
                 delimiter = !delimiter; // inverse for next tick
-            }
-
-            function checkTime(time) {
-                var day = time.day();
-                var hour = time.hour();
-
-                // Monday or Saturday - close and return
-                if (!(scope.isOpen = day > 0 && day < 6)) {
-                    scope.title = _INT("К сожалению сегодня - выходной день. Попробуйте позвонить к нам в понедельник или отправить запрос по электронной почте.");
-                    return;
-                }
-                // Time is between 9:00 - 18:00 - close and return
-                if (!(scope.isOpen = hour >= 9 && hour <= 17)) {
-                    if (hour > 17) {
-                        scope.title = _INT("Рабочий день завершён и мы не cможем принять ваш звонок. Отправьте свой запрос письмом по электронной почте.");
-                    } else if (hour < 9) {
-                        scope.title = _INT("Рабочий день еще не начался и мы не сможем принять ваш звонок. Позвоните попозже или отправьте свой запрос письмом по электронной почте.");
-                    }
-                    return;
-                }
-
-                scope.title = _INT("Мы готовы принять ваш звонок!");
             }
         }
     }
--- public/app/mainpage/pages/ContactsPage.controller.js
+++ public/app/mainpage/pages/ContactsPage.controller.js
@@ -9,24 +9,79 @@
 
     function ContactsPage(uiGmapGoogleMapApi, $modal) {
         var vm = this;
-        vm.openMap = openMap;
 
-        vm.latviaMap = {
-            markId: 'RigaOffice',
-            center: {
-                latitude: 55.692384,
-                longitude: 21.1515192
+        _.extend(vm, {
+            openMap,
+            check
+        });
+
+        function check(office) {
+            // return function as a handler
+            return function (time) {
+                var day = time.day();
+                var hour = time.hour();
+
+                // check for working day
+                if (!(vm[office].isOpen = day > 0 && day < 6)) {
+                    vm[office].title = _INT("К сожалению сегодня - выходной день. Попробуйте позвонить к нам в понедельник или отправить запрос по электронной почте.");
+                    return;
+                }
+
+                if (!(vm[office].isOpen = hour >= 9 && hour <= 17)) {
+                    if (hour > 17) {
+                        vm[office].title = _INT("Рабочий день завершён и мы не cможем принять ваш звонок. Отправьте свой запрос письмом по электронной почте.");
+                    } else if (hour < 9) {
+                        vm[office].title = _INT("Рабочий день еще не начался и мы не сможем принять ваш звонок. Позвоните попозже или отправьте свой запрос письмом по электронной почте.");
+                    }
+                    return;
+                }
+
+                vm[office].title = _INT("Мы готовы принять ваш звонок!");
+            }
+        }
+
+        function checkTime(time) {
+
+            // Monday or Saturday - close and return
+            if (!(scope.isOpen = day > 0 && day < 6)) {
+                scope.title = _INT("К сожалению сегодня - выходной день. Попробуйте позвонить к нам в понедельник или отправить запрос по электронной почте.");
+                return;
+            }
+            // Time is between 9:00 - 18:00 - close and return
+            if (!(scope.isOpen = hour >= 9 && hour <= 17)) {
+                if (hour > 17) {
+                    scope.title = _INT("Рабочий день завершён и мы не cможем принять ваш звонок. Отправьте свой запрос письмом по электронной почте.");
+                } else if (hour < 9) {
+                    scope.title = _INT("Рабочий день еще не начался и мы не сможем принять ваш звонок. Позвоните попозже или отправьте свой запрос письмом по электронной почте.");
+                }
+                return;
+            }
+
+            scope.title = _INT("Мы готовы принять ваш звонок!");
+        }
+
+        vm.latvia = {
+            map: {
+                markId: 'RigaOffice', zoom: 18,
+                center: {latitude: 55.692384, longitude: 21.1515192},
             },
-            zoom: 18
+            isOpen: true, title: ''
         };
 
-        vm.ukraineMap = {
-            markId: 'DneprOffice',
-            center: {
-                latitude: 48.4553585,
-                longitude: 35.043647
+        vm.ukraine = {
+            map: {
+                markId: 'DneprOffice', zoom: 18,
+                center: {latitude: 48.4553585, longitude: 35.043647}
             },
-            zoom: 18
+            isOpen: true, title: ''
+        };
+
+        vm.russia = {
+            isOpen: true, title: ''
+        };
+
+        vm.china = {
+            isOpen: true, title: ''
         };
 
         initController();
